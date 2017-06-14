715d489b74a7ca457c8b9d1e4b78d45e38a48738
canparlar <can.parlar@ggl.hisarschool.k12.tr>
2017-06-14 12:09:52 +0300

    cemersozupdated

--- Cem Ersoz/ass.c
+++ Cem Ersoz/ass.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-int palindrome (char sinput[40], char ssplitter[40]) {
+char sfirst[60];
+char ssecond[60];
 
-  char sfirst[20];
-  char ssecond[20];
+int split (char sinput[40], char ssplitter[40]) {
 
   char *search;
   search = strstr(sinput, ssplitter);
 
   if (search != NULL) {
-      //printf("Found string at index = %ld\n", search - sinput);
 
       strncpy(sfirst, sinput, (search-sinput));
       strncpy(ssecond, sinput+(search-sinput)+strlen(ssplitter)+1, strlen(sinput));
 
-      printf("First:%s\nSecond:%s\n",sfirst,ssecond);
-
-      return 0;
+      return 1;
   }
+
   else {
+      printf("No Splitter in Input\n");
       return 0;
   }
 }
 
+int list () {
+
+    char *ptr;
+
+    do {
+      ptr = strtok (sfirst," ,.");
+      printf ("%s\n",ptr);
+    } while (ptr != NULL)
+
+    return 1;
+}
+
+int translate () {
+
+
+
+}
 
  int main () {
 
-	 char sinput[40];
-   char ssplitter[40];
+	 char sinput[100];
+   char ssplitter[100];
 
-	 printf("Input String:");
+	 printf("Input:");
 	 fgets(sinput, sizeof(sinput), stdin);
 	 sinput[strlen(sinput)-1] = '\0';
 
@@ -38,7 +54,13 @@ int palindrome (char sinput[40], char ssplitter[40]) {
    fgets(ssplitter, sizeof(ssplitter), stdin);
    ssplitter[strlen(ssplitter)-1] = '\0';
 
-	 palindrome(sinput, ssplitter);
+	 split(sinput, ssplitter);
+
+   list();
+
+   translate();
+
+   printf("First:%s\nSecond:%s\n",sfirst,ssecond);
 
 	 return 0;
 }
